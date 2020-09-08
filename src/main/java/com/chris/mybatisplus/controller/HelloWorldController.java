111da26948eaba18f1e4123822cce5e87e574ced
chris <lilunlogic@163.com>
2020-09-08 16:27:12 +0800

    引用自定义springboot-starter

---
+++ src/main/java/com/chris/mybatisplus/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.chris.mybatisplus.controller;
+
+import com.chris.service.HelloWorldService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @Resource
+    private HelloWorldService helloWorldService;
+
+    @GetMapping("/sayHello")
+    public String helloworld(@RequestParam("info") String info) {
+        return helloWorldService.sayHello(info);
+    }
+
+}
