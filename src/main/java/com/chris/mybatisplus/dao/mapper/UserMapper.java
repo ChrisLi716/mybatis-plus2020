95e2023e65c064752ea0323e687e30004a96c866
chris <lilunlogic@163.com>
2022-08-28 17:31:01 +0800

    MyBatis 配置加密、解密

--- src/main/java/com/chris/mybatisplus/dao/mapper/UserMapper.java
+++ src/main/java/com/chris/mybatisplus/dao/mapper/UserMapper.java
@@ -1,12 +1,7 @@
 package com.chris.mybatisplus.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.chris.mybatisplus.dto.Encrypt;
 import com.chris.mybatisplus.entities.User;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.Date;
-import java.util.List;
 
 // 使用mybatis-plus增强接口
 public interface UserMapper extends BaseMapper<User> {
